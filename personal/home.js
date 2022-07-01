d0ee49684f00fa70fc2408fa44a41f57eee3a523
Jenna Scholl <bin19005@byui.edu>
2022-07-01 14:26:42 -0600

    added css

--- personal/home.js
+++ personal/home.js
@@ -18,40 +18,41 @@ function displayRecipe(data) {
   results = data;
   //for some reason the ingredients are not in an array on the API so I couldn't use a loop here
   const html = `<div id="recipe-box">                 
-                  <img src="${results.meals[0].strMealThumb}" alt="Image of ${results.meals[0].strMeal}">
-                </div>
-                <div id="description-box">
-                    <h2 id="recipe-name">${results.meals[0].strMeal}</h2>
-                    <input type="button" id="favorite" value="Add to Favorites">
-                    <input type="button" id="planner" value="Add to Planner">
-                </div>
-                <div id="cooking-info">
-                  <ui>
-                    <h3>Ingredients:</h3>
+                  <img id="recipe-photo" src="${results.meals[0].strMealThumb}" alt="Image of ${results.meals[0].strMeal}">
+                  <h2 id="recipe-name">${results.meals[0].strMeal}</h2>
+                  <h3 id="ingredients">Ingredients:</h3>
+                  <ul id="col-1">
                     <li>${results.meals[0].strMeasure1} ${results.meals[0].strIngredient1}</li>
-                    <li>${results.meals[0].strMeasure2} ${results.meals[0].strIngredient2}</li>
                     <li>${results.meals[0].strMeasure3} ${results.meals[0].strIngredient3}</li>
-                    <li>${results.meals[0].strMeasure4} ${results.meals[0].strIngredient4}</li>
                     <li>${results.meals[0].strMeasure5} ${results.meals[0].strIngredient5}</li>
-                    <li>${results.meals[0].strMeasure6} ${results.meals[0].strIngredient6}</li>
                     <li>${results.meals[0].strMeasure7} ${results.meals[0].strIngredient7}</li>
-                    <li>${results.meals[0].strMeasure8} ${results.meals[0].strIngredient8}</li>
                     <li>${results.meals[0].strMeasure9} ${results.meals[0].strIngredient9}</li>
-                    <li>${results.meals[0].strMeasure10} ${results.meals[0].strIngredient10}</li>
                     <li>${results.meals[0].strMeasure11} ${results.meals[0].strIngredient11}</li>
-                    <li>${results.meals[0].strMeasure12} ${results.meals[0].strIngredient12}</li>
                     <li>${results.meals[0].strMeasure13} ${results.meals[0].strIngredient13}</li>
-                    <li>${results.meals[0].strMeasure14} ${results.meals[0].strIngredient14}</li>
                     <li>${results.meals[0].strMeasure15} ${results.meals[0].strIngredient15}</li>
-                    <li>${results.meals[0].strMeasure16} ${results.meals[0].strIngredient16}</li>
                     <li>${results.meals[0].strMeasure17} ${results.meals[0].strIngredient17}</li>
-                    <li>${results.meals[0].strMeasure18} ${results.meals[0].strIngredient18}</li>
                     <li>${results.meals[0].strMeasure19} ${results.meals[0].strIngredient19}</li>
+                  </ul>
+                  <ul id="col-2">
+                    <li>${results.meals[0].strMeasure2} ${results.meals[0].strIngredient2}</li>
+                    <li>${results.meals[0].strMeasure4} ${results.meals[0].strIngredient4}</li>
+                    <li>${results.meals[0].strMeasure6} ${results.meals[0].strIngredient6}</li>
+                    <li>${results.meals[0].strMeasure8} ${results.meals[0].strIngredient8}</li>
+                    <li>${results.meals[0].strMeasure10} ${results.meals[0].strIngredient10}</li>
+                    <li>${results.meals[0].strMeasure12} ${results.meals[0].strIngredient12}</li>
+                    <li>${results.meals[0].strMeasure14} ${results.meals[0].strIngredient14}</li>
+                    <li>${results.meals[0].strMeasure16} ${results.meals[0].strIngredient16}</li>
+                    <li>${results.meals[0].strMeasure18} ${results.meals[0].strIngredient18}</li>
                     <li>${results.meals[0].strMeasure20} ${results.meals[0].strIngredient20}</li>
-                  </ui>
-                    <h3>Instructions:</h3>
+                  </ul>
+                  <input type="button" id="favorites-button" class="button" value="Add to Favorites">
+                  <input type="button" id="planner-button" class="button" value="Add to Planner">
+                  <h3 id="instructions-title">Instructions:</h3>
+                  <div id="instructions">
                     <p>${results.meals[0].strInstructions}</p>
+                  </div>
                 </div>`;
+
   outputElement.innerHTML = html;
   //logs data in the console, this was helpful to me while writing the program
   console.log("first: ", results);
