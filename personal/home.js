f4f51c63895febd683b40168cd79880d302f708d
Jenna Scholl <bin19005@byui.edu>
2022-07-05 10:30:01 -0600

    button and javascript functionality fix

--- personal/home.js
+++ personal/home.js
@@ -9,51 +9,101 @@ async function getRecipe() {
     // convert response
     let data = await response.json();
     displayRecipe(data);
+    results = data;
   }
+  
+};
+async function clearOutput() {
+  const output = document.querySelector('#output');
+  output.innerHTML = '';
+}
+function addToFavorites() {
+  window.location.href = "./favorites.html";
+  // displayRecipe(results);
+};
+
+function addToPlanner() {
+  window.location.href = "./meal-planning.html";
+  // displayRecipe(results);
 };
 
 //put data from API on the page
-function displayRecipe(data) {
-  const outputElement = document.querySelector("#output");
-  results = data;
-  //for some reason the ingredients are not in an array on the API so I couldn't use a loop here
-  const html = `<div id="recipe-box">                 
-                  <img id="recipe-photo" src="${results.meals[0].strMealThumb}" alt="Image of ${results.meals[0].strMeal}">
-                  <h2 id="recipe-name">${results.meals[0].strMeal}</h2>
-                  <h3 id="ingredients">Ingredients:</h3>
-                  <ul id="col-1">
-                    <li>${results.meals[0].strMeasure1} ${results.meals[0].strIngredient1}</li>
-                    <li>${results.meals[0].strMeasure3} ${results.meals[0].strIngredient3}</li>
-                    <li>${results.meals[0].strMeasure5} ${results.meals[0].strIngredient5}</li>
-                    <li>${results.meals[0].strMeasure7} ${results.meals[0].strIngredient7}</li>
-                    <li>${results.meals[0].strMeasure9} ${results.meals[0].strIngredient9}</li>
-                    <li>${results.meals[0].strMeasure11} ${results.meals[0].strIngredient11}</li>
-                    <li>${results.meals[0].strMeasure13} ${results.meals[0].strIngredient13}</li>
-                    <li>${results.meals[0].strMeasure15} ${results.meals[0].strIngredient15}</li>
-                    <li>${results.meals[0].strMeasure17} ${results.meals[0].strIngredient17}</li>
-                    <li>${results.meals[0].strMeasure19} ${results.meals[0].strIngredient19}</li>
-                  </ul>
-                  <ul id="col-2">
-                    <li>${results.meals[0].strMeasure2} ${results.meals[0].strIngredient2}</li>
-                    <li>${results.meals[0].strMeasure4} ${results.meals[0].strIngredient4}</li>
-                    <li>${results.meals[0].strMeasure6} ${results.meals[0].strIngredient6}</li>
-                    <li>${results.meals[0].strMeasure8} ${results.meals[0].strIngredient8}</li>
-                    <li>${results.meals[0].strMeasure10} ${results.meals[0].strIngredient10}</li>
-                    <li>${results.meals[0].strMeasure12} ${results.meals[0].strIngredient12}</li>
-                    <li>${results.meals[0].strMeasure14} ${results.meals[0].strIngredient14}</li>
-                    <li>${results.meals[0].strMeasure16} ${results.meals[0].strIngredient16}</li>
-                    <li>${results.meals[0].strMeasure18} ${results.meals[0].strIngredient18}</li>
-                    <li>${results.meals[0].strMeasure20} ${results.meals[0].strIngredient20}</li>
-                  </ul>
-                  <input type="button" id="favorites-button" class="button" value="Add to Favorites">
-                  <input type="button" id="planner-button" class="button" value="Add to Planner">
-                  <h3 id="instructions-title">Instructions:</h3>
-                  <div id="instructions">
-                    <p>${results.meals[0].strInstructions}</p>
-                  </div>
-                </div>`;
+function displayRecipe(results) {
+  const output = document.querySelector('#output');
+  const recipeCard = document.createElement('div');
+  recipeCard.id = "recipe-box";
+  output.appendChild(recipeCard);
 
-  outputElement.innerHTML = html;
+  const recipeName = document.createElement('h2');
+  recipeName.innerHTML = `${results.meals[0].strMeal}`;
+  recipeName.id = "recipe-name";
+  recipeCard.appendChild(recipeName);
+
+  const ingredientsTitle = document.createElement('h3');
+  ingredientsTitle.innerHTML = 'Ingredients:';
+  ingredientsTitle.id = "ingredients-title";
+  recipeCard.appendChild(ingredientsTitle);
+
+  const ingredients1 = document.createElement('ul');
+  ingredients1.innerHTML = `<li>${results.meals[0].strMeasure1} ${results.meals[0].strIngredient1}</li>
+    <li>${results.meals[0].strMeasure3} ${results.meals[0].strIngredient3}</li>
+    <li>${results.meals[0].strMeasure5} ${results.meals[0].strIngredient5}</li>
+    <li>${results.meals[0].strMeasure7} ${results.meals[0].strIngredient7}</li>
+    <li>${results.meals[0].strMeasure9} ${results.meals[0].strIngredient9}</li>
+    <li>${results.meals[0].strMeasure11} ${results.meals[0].strIngredient11}</li>
+    <li>${results.meals[0].strMeasure13} ${results.meals[0].strIngredient13}</li>
+    <li>${results.meals[0].strMeasure15} ${results.meals[0].strIngredient15}</li>
+    <li>${results.meals[0].strMeasure17} ${results.meals[0].strIngredient17}</li>
+    <li>${results.meals[0].strMeasure19} ${results.meals[0].strIngredient19}</li>`;
+  ingredients1.id = 'col-1';
+  recipeCard.appendChild(ingredients1);
+
+  const ingredients2 = document.createElement('ul');
+  ingredients2.innerHTML = `<li>${results.meals[0].strMeasure2} ${results.meals[0].strIngredient2}</li>
+    <li>${results.meals[0].strMeasure4} ${results.meals[0].strIngredient4}</li>
+    <li>${results.meals[0].strMeasure6} ${results.meals[0].strIngredient6}</li>
+    <li>${results.meals[0].strMeasure8} ${results.meals[0].strIngredient8}</li>
+    <li>${results.meals[0].strMeasure10} ${results.meals[0].strIngredient10}</li>
+    <li>${results.meals[0].strMeasure12} ${results.meals[0].strIngredient12}</li>
+    <li>${results.meals[0].strMeasure14} ${results.meals[0].strIngredient14}</li>
+    <li>${results.meals[0].strMeasure16} ${results.meals[0].strIngredient16}</li>
+    <li>${results.meals[0].strMeasure18} ${results.meals[0].strIngredient18}</li>
+    <li>${results.meals[0].strMeasure20} ${results.meals[0].strIngredient20}</li>`;
+  ingredients2.id = 'col-2';
+  recipeCard.appendChild(ingredients2);
+
+  const recipePhoto = document.createElement('img');
+  recipePhoto.id = 'recipe-photo';
+  recipePhoto.src = `${results.meals[0].strMealThumb}`;
+  recipePhoto.alt = 'thumbnail';
+  recipeCard.appendChild(recipePhoto);
+  
+  const favButton = document.createElement('input');
+  favButton.type = 'button';
+  favButton.id = 'favorites-button';
+  favButton.className = "button";
+  favButton.value = 'Add to Favorites';
+  favButton.addEventListener('click', addToFavorites);
+  recipeCard.appendChild(favButton);
+
+  const plannerButton = document.createElement('input');
+  plannerButton.type = 'button';
+  plannerButton.id = 'planner-button';
+  plannerButton.className = 'button';
+  plannerButton.value = 'Add to Planner';
+  plannerButton.addEventListener('click', addToPlanner);
+  recipeCard.appendChild(plannerButton);
+
+  const instructionsTitle = document.createElement('h3');
+  instructionsTitle.innerHTML = 'Instructions:';
+  instructionsTitle.id = 'instructions-title';
+  recipeCard.appendChild(instructionsTitle);
+
+  const instructions = document.createElement('p');
+  instructions.innerHTML = `${results.meals[0].strInstructions}`;
+  instructions.id = "instructions";
+  recipeCard.appendChild(instructions);
+
   window.scrollTo({
     top: 600,
     left: 0,
@@ -61,14 +111,10 @@ function displayRecipe(data) {
   });
   //logs data in the console, this was helpful to me while writing the program
   console.log("first: ", results);
+
 };
 
 //run function getRecipe when the generate button is clicked
+document.querySelector('#generate').addEventListener('click', clearOutput);
 document.querySelector('#generate').addEventListener('click', getRecipe);
-
-//adds recipe to favorites page
-document.queerySelector('#favorites-button').addEventListener('click', getRecipe);
-
-//gets current year for copyright in the footer
-const currentDate = new Date();
-document.querySelector('#year').textContent = currentDate.getFullYear();
+  //adds recipe to favorites page
